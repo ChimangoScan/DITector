65dc549221c1ef3a8b06040896b82842e254a84e
MussoMac <2020832585@qq.com>
2023-03-09 20:22:13 +0800

    add some comments

--- crawler/scrape.go
+++ crawler/scrape.go
@@ -16,9 +16,29 @@ func ScrapeRegRepoListRecursive(c *colly.Collector, keyword, source string) {
 }
 
 // ScrapeRepoInfo 用于爬取指定repo的metadata和全部tag的信息。
-// 考虑进一步将metadata和tag持久化。
+// 考虑在内部进一步将metadata和tag持久化。
 func ScrapeRepoInfo(namespace, repo string) {
+	// 思路1-------------------------
+	// 建立有效管道每阶段都在传数据
+	// ch1 := make(chan Repository__)
+	// ScrapeRepoMetadata 爬Metadata，结果传进ch1
 
+	// 读ch1
+	// ch2 := make(chan TagReceiver__ 收tag list)
+	// ScrapeRepoTagsRecursive爬tag list传进ch2
+
+	// 思路2-------------------------
+	// GetCollector时候传入&Repository__
+	// ch := make(chan TagReceiver__ 收tag list)
+	// ScrapeRepoTagsRecursive爬tag list传进ch
+	// 读ch
+	// for t := range ch {
+	// 	for _, tag := range t.Results {
+	// 		进一步爬每个tag的Archs
+	//
+	//	}
+	// }
+	// 后续都在这个基础上
 }
 
 // ScrapeRepoMetadata 用于爬取指定repo的metadata，返回一个。
